826c40a023443f9370243c7fe2a1713c062846bb
aeckelineravalomanda <79484240+aeckelineravalomanda@users.noreply.github.com>
2022-05-16 14:50:21 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -3,13 +3,8 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "West Europe"
-}
-
 module "network" {
   source  = "app.terraform.io/aeckrava/network/azurerm"
   version = "3.5.0"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
